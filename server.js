304c32f4146c5d32aea5ce2423ec30b11bc40526
utkarshtewari <utkarshtewari@gmail.com>
2017-06-13 07:54:41 +0000

    first commit

--- server.js
+++ server.js
@@ -4,7 +4,16 @@ var mongoose=require('mongoose');
 var app = express();
 app.use(bodyParser.json());
 mongoose.connect('mongodb://localhost:27017/mittens');
-
+app.use(express.static('public'));
+var Meow = mongoose.model('Meow', {
+ text: String()
+  });
+app.get('/meows',function(req,res,next){
+  Meow.find({}, function (err, meows){
+    return res.json(meows);
+  });
+} );
+ 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
